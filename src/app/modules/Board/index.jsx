ea65f8f2702eb5cccc6274d54f9ae787baafd926
Abdul-Basit-Ansari <abdulbasitansari288@gmail.com>
2023-10-12 14:51:13 +0500

    disable eslist

--- src/app/modules/Board/index.jsx
+++ src/app/modules/Board/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
