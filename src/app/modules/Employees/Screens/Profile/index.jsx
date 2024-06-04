d087d72fa163c7dd656ade2ed6a00ad63d52ab61
ajwatanveer <aleezy.tanv53@gmail.com>
2024-06-04 21:33:52 +0500

    Rewampping Edit Banking Detail Form

--- src/app/modules/Employees/Screens/Profile/index.jsx
+++ src/app/modules/Employees/Screens/Profile/index.jsx
@@ -1,11 +1,9 @@
-import BankInformation from './BankInformation';
 import PersonalInformation from './PersonalInformation';
 import EditEmployeeProfile from './EditEmployeeProfile'
 import ExperienceInformation from './ExperienceInformation';
 
 
 export{
-    BankInformation,
     PersonalInformation,
     EditEmployeeProfile,
     ExperienceInformation,
