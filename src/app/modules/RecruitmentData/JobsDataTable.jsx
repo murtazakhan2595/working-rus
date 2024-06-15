99f6d21cc713c5a661ce6fbb017d9b4f58218cd0
developerUmair <umairmak143@gmail.com>
2024-06-15 04:52:46 -0700

    job description page designing and integeration

--- src/app/modules/RecruitmentData/JobsDataTable.jsx
+++ src/app/modules/RecruitmentData/JobsDataTable.jsx
@@ -10,6 +10,7 @@ import dots from "../../../assets/images/dots.svg";
 import ViewJobDetails from "./ViewJobDetails";
 import Tabs from "./Sections/Tabs";
 import { fetchJobPosts } from "../../hooks/recruitment";
+import { LuExternalLink } from "react-icons/lu";
 
 const JobsDataTable = ({ baseUrl, token }) => {
   const [posts, setPosts] = useState([]);
@@ -26,7 +27,7 @@ const JobsDataTable = ({ baseUrl, token }) => {
         const data = await fetchJobPosts(baseUrl, token, activeTab);
         setPosts(data);
       } catch (error) {
-        console.error('Error fetching posts:', error);
+        console.error("Error fetching posts:", error);
       } finally {
         setLoading(false);
       }
@@ -36,14 +37,17 @@ const JobsDataTable = ({ baseUrl, token }) => {
   }, [activeTab, baseUrl, token]);
 
   const formatDate = (dateString) => {
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-    const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    const formattedDate = new Date(dateString).toLocaleDateString(
+      undefined,
+      options
+    );
     return formattedDate;
   };
 
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
-    toast.success('Link copied to clipboard!', {
+    toast.success("Link copied to clipboard!", {
       position: toast.POSITION.TOP_RIGHT,
       autoClose: 1000,
     });
@@ -66,39 +70,66 @@ const JobsDataTable = ({ baseUrl, token }) => {
           ) : (
             <tbody className="bg-white text-gray-500">
               {posts.map((post) => (
-                <tr className={`whitespace-nowrap border-b-2 hover:bg-gray-100`} key={post.id}>
+                <tr
+                  className={`whitespace-nowrap border-b-2 hover:bg-gray-100`}
+                  key={post.id}
+                >
                   <td className="px-4 py-3">
                     <div className="flex flex-col justify-between gap-y-10">
                       <div className="flex justify-between">
                         <div className="flex items-center gap-x-2">
                           <img src={jobIcon} alt="" />
                           <div>
-                            <p className="font-lato text-baseGray text-base">{post.id}</p>
-                            <h3 className="font-lato text-[20px] text-baseGray font-bold">{post.Job_Title}</h3>
+                            <p className="font-lato text-baseGray text-base">
+                              {post.id}
+                            </p>
+                            <h3 className="font-lato text-[20px] text-baseGray font-bold">
+                              {post.Job_Title}
+                            </h3>
                           </div>
                         </div>
                         <div className="font-lato text-base text-baseGray flex items-center gap-x-4">
-                          <Link to={`/applicants/${post.id}`} className="border px-3 py-2 rounded-md border-gray-400">
+                          <Link
+                            to={`/applicants/${post.id}`}
+                            className="border px-3 py-2 rounded-md border-gray-400"
+                          >
                             View applications
                           </Link>
-                          <img src={dots} alt="" onClick={() => handleDotsClick(post)} className="cursor-pointer" />
+                          <Link
+                            to={`/job-description/${post.id}`}
+                            className="px-3 py-2"
+                          >
+                            <LuExternalLink />
+                          </Link>
+                          <img
+                            src={dots}
+                            alt=""
+                            onClick={() => handleDotsClick(post)}
+                            className="cursor-pointer"
+                          />
                         </div>
                       </div>
 
                       <div className="flex items-center justify-between">
                         <div className="font-lato text-base text-baseGray flex items-center gap-x-2">
                           <IoCalendarOutline className="text-lg" />
-                          {`${formatDate(post.updated_at)} to ${formatDate(post.Deadline)}`}
+                          {`${formatDate(post.updated_at)} to ${formatDate(
+                            post.Deadline
+                          )}`}
                         </div>
                         <div className="flex justify-between items-center gap-x-2">
                           <div
                             className={`flex items-center text-baseGray font-lato text-base font-normal rounded-2xl px-2 ${
-                              post.status === 'live' ? 'bg-green-100' : 'bg-red-100'
+                              post.status === "live"
+                                ? "bg-green-100"
+                                : "bg-red-100"
                             }`}
                           >
                             <span
                               className={`w-3 h-3 rounded-full mr-2 ${
-                                post.status === 'live' ? 'bg-green-500' : 'bg-red-500'
+                                post.status === "live"
+                                  ? "bg-green-500"
+                                  : "bg-red-500"
                               }`}
                             ></span>
                             {post.status}
@@ -130,9 +161,7 @@ const JobsDataTable = ({ baseUrl, token }) => {
 
   return (
     <div className="flex w-full flex-col bg-[#F0F1F2] h-[100vh] p-2">
-      <Tabs tabs={["All", "Open", "Closed"]}
-        onTabChange={setActiveTab}
-      />
+      <Tabs tabs={["All", "Open", "Closed"]} onTabChange={setActiveTab} />
       {renderTable()}
       {selectedPost && (
         <ViewJobDetails post={selectedPost} onClose={closeModal} />
