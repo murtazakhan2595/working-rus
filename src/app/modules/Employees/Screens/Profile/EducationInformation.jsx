9b2084d36b5177fcabcd303369b311ecf74d8e70
developerUmair <umairmak143@gmail.com>
2024-07-04 19:16:47 +0500

    remove certifications, education sections and experiences sections

--- src/app/modules/Employees/Screens/Profile/EducationInformation.jsx
+++ src/app/modules/Employees/Screens/Profile/EducationInformation.jsx
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  Row,
-  Col,
-  Button,
-  Form,
-} from "reactstrap";
+import { Row, Col, Form } from "reactstrap";
 import { Formik } from "formik";
 import { connect } from "react-redux";
 import {
@@ -23,6 +18,7 @@ import {
 } from "components/form-control";
 import { educationTypeOptions } from "data/Data.js";
 import { Link } from "react-router-dom";
+import { FaTimes } from "react-icons/fa"; // Import the close icon from react-icons
 
 const EducationInformation = ({
   nextstep,
@@ -39,7 +35,6 @@ const EducationInformation = ({
   useEffect(() => {
     getEmployeeAcademicRecordData(employeeId)
       .then((response) => {
-        // Process the response to include the document name
         setEducations(response);
         setIsLoading(false);
       })
@@ -89,11 +84,20 @@ const EducationInformation = ({
                       props.values.educations.length > 0 &&
                       props.values.educations.map((education, index) => (
                         <React.Fragment key={index}>
-                          {console.log(education)}
                           <Col md="12">
-                            <h5 className="fw-700 mb-3 mt-4">
-                              Education {index + 1}
-                            </h5>
+                            <div className="d-flex justify-content-between align-items-center">
+                              <h5 className="fw-700 mb-3 mt-4">
+                                Education {index + 1}
+                              </h5>
+                              <FaTimes
+                                className="cursor-pointer"
+                                onClick={() => {
+                                  const newEducations = [...props.values.educations];
+                                  newEducations.splice(index, 1);
+                                  props.setFieldValue('educations', newEducations);
+                                }}
+                              />
+                            </div>
                           </Col>
                           <div className="flex flex-wrap gap-x-3">
                             <div className="w-full md:w-[48%] z-0">
@@ -277,3 +281,284 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(EducationInformation);
+
+
+// import React, { useState, useEffect } from "react";
+// import {
+//   Row,
+//   Col,
+//   Button,
+//   Form,
+// } from "reactstrap";
+// import { Formik } from "formik";
+// import { connect } from "react-redux";
+// import {
+//   getEmployeeAcademicRecordData,
+//   saveEmployeeAcademicRecordData,
+// } from "app/hooks/employee.jsx";
+// import PageLoader from "components/PageLoader.jsx";
+// import { EmployeeAcademicRecord } from "app/utils/Types/Employee";
+// import {
+//   DateInput,
+//   SelectComponent,
+//   TextInput,
+//   CustomDarkButton,
+//   CustomLightOutlineButton,
+//   FileInput,
+// } from "components/form-control";
+// import { educationTypeOptions } from "data/Data.js";
+// import { Link } from "react-router-dom";
+
+// const EducationInformation = ({
+//   nextstep,
+//   baseUrl,
+//   token,
+//   employeeId,
+//   isEditMode,
+//   prevStep,
+// }) => {
+//   const formRef = React.createRef();
+//   const [educations, setEducations] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     getEmployeeAcademicRecordData(employeeId)
+//       .then((response) => {
+//         // Process the response to include the document name
+//         setEducations(response);
+//         setIsLoading(false);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, [baseUrl, employeeId, token]);
+
+//   const handleSubmit = async (data) => {
+//     try {
+//       const response = await saveEmployeeAcademicRecordData(
+//         employeeId,
+//         data.educations
+//       );
+//       if (response) nextstep();
+//     } catch (error) {
+//       console.error("Error saving academic records:", error);
+//     }
+//   };
+
+//   return (
+//     <>
+//       {isLoading ? (
+//         <Row>
+//           <Col lg={12}>
+//             <PageLoader />
+//           </Col>
+//         </Row>
+//       ) : (
+//         <Row>
+//           <Col lg={12}>
+//             <Formik
+//               initialValues={{ educations: educations }}
+//               ref={formRef}
+//               onSubmit={(values, { resetForm }) => {
+//                 handleSubmit(values);
+//               }}
+//               validate={(values) => {
+//                 const errors = {};
+//                 return errors;
+//               }}
+//             >
+//               {(props) => (
+//                 <Form onSubmit={props.handleSubmit}>
+//                   <Row>
+//                     {props.values?.educations &&
+//                       props.values.educations.length > 0 &&
+//                       props.values.educations.map((education, index) => (
+//                         <React.Fragment key={index}>
+//                           {console.log(education)}
+//                           <Col md="12">
+//                             <h5 className="fw-700 mb-3 mt-4">
+//                               Education {index + 1}
+//                             </h5>
+//                           </Col>
+//                           <div className="flex flex-wrap gap-x-3">
+//                             <div className="w-full md:w-[48%] z-0">
+//                               <SelectComponent
+//                                 options={educationTypeOptions}
+//                                 name={`educations[${index}].education_level`}
+//                                 value={education.education_level}
+//                                 onChange={(field, value) => {
+//                                   props.setFieldValue(field, value);
+//                                 }}
+//                                 label={"Educational Level"}
+//                                 required
+//                                 error={
+//                                   props.errors.educations &&
+//                                   props.errors.educations[index]
+//                                     ?.education_level
+//                                 }
+//                                 touched={
+//                                   props.touched.educations &&
+//                                   props.touched.educations[index]
+//                                     ?.education_level
+//                                 }
+//                               />
+//                             </div>
+//                             <div className="w-full md:w-[48%]">
+//                               <TextInput
+//                                 name={`educations[${index}].program`}
+//                                 value={education.program}
+//                                 onChange={(field, value) => {
+//                                   props.setFieldValue(field, value);
+//                                 }}
+//                                 label={"Program"}
+//                                 required
+//                                 error={
+//                                   props.errors.educations &&
+//                                   props.errors.educations[index]?.program
+//                                 }
+//                                 touched={
+//                                   props.touched.educations &&
+//                                   props.touched.educations[index]?.program
+//                                 }
+//                               />
+//                             </div>
+//                             <div className="w-full md:w-[48%]">
+//                               <TextInput
+//                                 name={`educations[${index}].institute_name`}
+//                                 value={education.institute_name}
+//                                 onChange={(field, value) => {
+//                                   props.setFieldValue(field, value);
+//                                 }}
+//                                 label={"Institute"}
+//                                 required
+//                                 error={
+//                                   props.errors.educations &&
+//                                   props.errors.educations[index]?.institute_name
+//                                 }
+//                                 touched={
+//                                   props.touched.educations &&
+//                                   props.touched.educations[index]
+//                                     ?.institute_name
+//                                 }
+//                               />
+//                             </div>
+//                             <div className="w-full md:w-[48%]">
+//                               <DateInput
+//                                 name={`educations[${index}].edu_start_date`}
+//                                 value={education.edu_start_date}
+//                                 onChange={(field, value) => {
+//                                   props.setFieldValue(field, value);
+//                                 }}
+//                                 label={"Start Date"}
+//                                 required
+//                                 error={
+//                                   props.errors.educations &&
+//                                   props.errors.educations[index]?.edu_start_date
+//                                 }
+//                                 touched={
+//                                   props.touched.educations &&
+//                                   props.touched.educations[index]
+//                                     ?.edu_start_date
+//                                 }
+//                               />
+//                             </div>
+//                             <div className="w-full md:w-[48%]">
+//                               <DateInput
+//                                 name={`educations[${index}].edu_end_date`}
+//                                 value={education.edu_end_date}
+//                                 onChange={(field, value) => {
+//                                   props.setFieldValue(field, value);
+//                                 }}
+//                                 label={"End Date"}
+//                                 required
+//                                 error={
+//                                   props.errors.educations &&
+//                                   props.errors.educations[index]?.edu_end_date
+//                                 }
+//                                 touched={
+//                                   props.touched.educations &&
+//                                   props.touched.educations[index]?.edu_end_date
+//                                 }
+//                               />
+//                             </div>
+//                             <div className="w-full">
+//                               <FileInput
+//                                 acceptType=".pdf"
+//                                 name={`educations[${index}].education_body`}
+//                                 value={education.education_body}
+//                                 onChange={(field, value) => {
+//                                   props.setFieldValue(field, value);
+//                                 }}
+//                                 label={"Certification or drag it here"}
+//                                 required
+//                                 error={
+//                                   props.errors.educations &&
+//                                   props.errors.educations[index]?.education_body
+//                                 }
+//                                 touched={
+//                                   props.touched.educations &&
+//                                   props.touched.educations[index]
+//                                     ?.education_body
+//                                 }
+//                               />
+//                             </div>
+//                           </div>
+//                         </React.Fragment>
+//                       ))}
+//                     <Col md="12" className="text-left">
+//                       <Link
+//                         type="button"
+//                         className="btn btn-outline-dark"
+//                         onClick={() => {
+//                           const length = props.values?.educations?.length;
+//                           const index = length ? length : 0;
+//                           props.setFieldValue(
+//                             `educations[${index}]`,
+//                             EmployeeAcademicRecord
+//                           );
+//                         }}
+//                       >
+//                         + Add Another
+//                       </Link>
+//                     </Col>
+//                   </Row>
+//                   <hr />
+//                   <Row>
+//                     <Col md={6} className="text-left">
+//                       {!isEditMode && (
+//                         <CustomLightOutlineButton
+//                           onClick={() => {
+//                             prevStep();
+//                           }}
+//                           label={"Back"}
+//                         />
+//                       )}
+//                     </Col>
+//                     <Col md="6" className="text-right">
+//                       <CustomDarkButton
+//                         onClick={() => {
+//                           props.handleSubmit();
+//                         }}
+//                         label={isEditMode ? "Save" : "Next"}
+//                       />
+//                     </Col>
+//                   </Row>
+//                 </Form>
+//               )}
+//             </Formik>
+//           </Col>
+//         </Row>
+//       )}
+//     </>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     userProfile: state.user.userProfile,
+//     token: state.user.token,
+//     baseUrl: state.user.baseUrl,
+//   };
+// };
+
+// export default connect(mapStateToProps)(EducationInformation);
--- src/app/modules/Employees/Screens/Profile/ExperienceInformation.jsx
+++ src/app/modules/Employees/Screens/Profile/ExperienceInformation.jsx
@@ -3,25 +3,15 @@ import { Row, Col, Form } from "reactstrap";
 import { Formik } from "formik";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import {
-  getEmployeeProfessionalExperianceData,
-  saveEmployeeProfessionalExperianceData,
-} from "app/hooks/employee.jsx";
+import { getEmployeeProfessionalExperianceData, saveEmployeeProfessionalExperianceData } from "app/hooks/employee.jsx";
 import PageLoader from "components/PageLoader.jsx";
 import Experience from "../Sections/ExperianceForm.jsx";
 import { EmployeeProfessionalExperiance } from "app/utils/Types/Employee";
-import {
-  CustomDarkButton,
-  CustomLightOutlineButton,
-} from "components/form-control";
+import { CustomDarkButton, CustomLightOutlineButton } from "components/form-control";
 import { validationEmployeeExperienceFormSchema } from "app/utils/FormSchema/employeeFormSchema.jsx";
+import { FaTimes } from "react-icons/fa"; // Import the close icon from react-icons
 
-const ExperienceInformation = ({
-  nextstep,
-  employeeId,
-  isEditMode,
-  prevStep,
-}) => {
+const ExperienceInformation = ({ nextstep, employeeId, isEditMode, prevStep }) => {
   const formRef = React.createRef();
   const [experiences, setExperiences] = useState({});
   const [isLoading, setIsLoading] = useState(true);
@@ -36,11 +26,9 @@ const ExperienceInformation = ({
         console.log(error);
       });
   }, [employeeId]);
+
   const handleSubmit = async (data) => {
-    const response = await saveEmployeeProfessionalExperianceData(
-      employeeId,
-      data.experiences
-    );
+    const response = await saveEmployeeProfessionalExperianceData(employeeId, data.experiences);
     if (response) nextstep();
   };
 
@@ -74,31 +62,30 @@ const ExperienceInformation = ({
                       props.values.experiences.map((experience, index) => (
                         <React.Fragment key={index}>
                           <Col md="12">
-                            <h5 className="fw-700 mb-3 mt-4">
-                              Experience {index + 1}
-                            </h5>
+                            <div className="d-flex justify-content-between align-items-center">
+                              <h5 className="fw-700 mb-3 mt-4">
+                                Experience {index + 1}
+                              </h5>
+                              <FaTimes
+                                className="cursor-pointer"
+                                onClick={() => {
+                                  const newExperiences = [...props.values.experiences];
+                                  newExperiences.splice(index, 1);
+                                  props.setFieldValue('experiences', newExperiences);
+                                }}
+                              />
+                            </div>
                           </Col>
                           <Experience
                             values={experience}
-                            errors={
-                              props.errors?.experiences
-                                ? props.errors?.experiences[index]
-                                : {}
-                            }
-                            touched={
-                              props.touched?.experiences
-                                ? props.touched?.experiences[index]
-                                : {}
-                            }
+                            errors={props.errors?.experiences ? props.errors?.experiences[index] : {}}
+                            touched={props.touched?.experiences ? props.touched?.experiences[index] : {}}
                             onChange={(field, value) => {
                               experience[field] = value;
                               if (field === "disableEndDate" && value) {
                                 experience.exp_end_date = null;
                               }
-                              props.setFieldValue(
-                                `experiences[${index}]`,
-                                experience
-                              );
+                              props.setFieldValue(`experiences[${index}]`, experience);
                             }}
                           />
                         </React.Fragment>
@@ -111,10 +98,7 @@ const ExperienceInformation = ({
                           const length = props.values?.experiences?.length;
                           const index = length ? length : 0;
 
-                          props.setFieldValue(
-                            `experiences[${index}]`,
-                            EmployeeProfessionalExperiance
-                          );
+                          props.setFieldValue(`experiences[${index}]`, EmployeeProfessionalExperiance);
                         }}
                       >
                         + Add Another
@@ -161,3 +145,168 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps)(ExperienceInformation);
+
+
+// import React, { useState, useEffect } from "react";
+// import { Row, Col, Form } from "reactstrap";
+// import { Formik } from "formik";
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+// import {
+//   getEmployeeProfessionalExperianceData,
+//   saveEmployeeProfessionalExperianceData,
+// } from "app/hooks/employee.jsx";
+// import PageLoader from "components/PageLoader.jsx";
+// import Experience from "../Sections/ExperianceForm.jsx";
+// import { EmployeeProfessionalExperiance } from "app/utils/Types/Employee";
+// import {
+//   CustomDarkButton,
+//   CustomLightOutlineButton,
+// } from "components/form-control";
+// import { validationEmployeeExperienceFormSchema } from "app/utils/FormSchema/employeeFormSchema.jsx";
+
+// const ExperienceInformation = ({
+//   nextstep,
+//   employeeId,
+//   isEditMode,
+//   prevStep,
+// }) => {
+//   const formRef = React.createRef();
+//   const [experiences, setExperiences] = useState({});
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     getEmployeeProfessionalExperianceData(employeeId)
+//       .then((response) => {
+//         setExperiences(response);
+//         setIsLoading(false);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, [employeeId]);
+//   const handleSubmit = async (data) => {
+//     const response = await saveEmployeeProfessionalExperianceData(
+//       employeeId,
+//       data.experiences
+//     );
+//     if (response) nextstep();
+//   };
+
+//   return (
+//     <>
+//       {isLoading ? (
+//         <Row>
+//           <Col lg={12}>
+//             <PageLoader />
+//           </Col>
+//         </Row>
+//       ) : (
+//         <Row>
+//           <Col lg={12}>
+//             <Formik
+//               initialValues={{ experiences: experiences }}
+//               ref={formRef}
+//               onSubmit={(values, { resetForm }) => {
+//                 handleSubmit(values, resetForm);
+//               }}
+//               validate={(values) => {
+//                 const errors = validationEmployeeExperienceFormSchema(values);
+//                 return errors;
+//               }}
+//             >
+//               {(props) => (
+//                 <Form onSubmit={props.handleSubmit}>
+//                   <Row>
+//                     {props.values?.experiences &&
+//                       props.values.experiences.length > 0 &&
+//                       props.values.experiences.map((experience, index) => (
+//                         <React.Fragment key={index}>
+//                           <Col md="12">
+//                             <h5 className="fw-700 mb-3 mt-4">
+//                               Experience {index + 1}
+//                             </h5>
+//                           </Col>
+//                           <Experience
+//                             values={experience}
+//                             errors={
+//                               props.errors?.experiences
+//                                 ? props.errors?.experiences[index]
+//                                 : {}
+//                             }
+//                             touched={
+//                               props.touched?.experiences
+//                                 ? props.touched?.experiences[index]
+//                                 : {}
+//                             }
+//                             onChange={(field, value) => {
+//                               experience[field] = value;
+//                               if (field === "disableEndDate" && value) {
+//                                 experience.exp_end_date = null;
+//                               }
+//                               props.setFieldValue(
+//                                 `experiences[${index}]`,
+//                                 experience
+//                               );
+//                             }}
+//                           />
+//                         </React.Fragment>
+//                       ))}
+//                     <Col md="12" className="text-left mt-4">
+//                       <Link
+//                         type="button"
+//                         className="btn btn-outline-dark"
+//                         onClick={() => {
+//                           const length = props.values?.experiences?.length;
+//                           const index = length ? length : 0;
+
+//                           props.setFieldValue(
+//                             `experiences[${index}]`,
+//                             EmployeeProfessionalExperiance
+//                           );
+//                         }}
+//                       >
+//                         + Add Another
+//                       </Link>
+//                     </Col>
+//                   </Row>
+//                   <hr />
+//                   <Row>
+//                     <Col md={6} className="text-left">
+//                       {!isEditMode && (
+//                         <CustomLightOutlineButton
+//                           onClick={() => {
+//                             prevStep();
+//                           }}
+//                           label={"Back"}
+//                         />
+//                       )}
+//                     </Col>
+//                     <Col md="6" className="text-right">
+//                       <CustomDarkButton
+//                         onClick={() => {
+//                           props.handleSubmit();
+//                         }}
+//                         label={isEditMode ? "Save" : "Next"}
+//                       />
+//                     </Col>
+//                   </Row>
+//                 </Form>
+//               )}
+//             </Formik>
+//           </Col>
+//         </Row>
+//       )}
+//     </>
+//   );
+// };
+
+// const mapStateToProps = (state) => {
+//   return {
+//     userProfile: state.user.userProfile,
+//     token: state.user.token,
+//     baseUrl: state.user.baseUrl,
+//   };
+// };
+
+// export default connect(mapStateToProps)(ExperienceInformation);
