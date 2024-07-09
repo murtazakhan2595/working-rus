9b69e0b3e62eb2409cfa92c5ea80085274eafa73
developerUmair <umairmak143@gmail.com>
2024-07-09 14:41:52 +0500

    project screens and its minor integerations

--- src/app/modules/TaskManagment/Projects/Projects.jsx
+++ src/app/modules/TaskManagment/Projects/Projects.jsx
@@ -8,6 +8,11 @@ import { FilterInput } from "components/form-control";
 import { Card, CardHeader, CardBody, Row, Col } from "reactstrap";
 import { CiCirclePlus } from "react-icons/ci";
 import ProjectModel from "./CreateProjectModel";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import moment from "moment";
+import { getRandomColor } from "utils/getValuesFromTables";
+import { EmployeeName } from "utils/getValuesFromTables";
+import CustomDropdown from "../sections/CutsomDropdown";
 
 const Projects = ({ userProfile }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -96,21 +101,75 @@ const Projects = ({ userProfile }) => {
 };
 
 const RenderProject = ({ project, toggleAddProject }) => {
+  const projectMembers = project?.project_members || [];
+  const displayedMembers = projectMembers.slice(0, 3);
+  const remainingCount = projectMembers.length - displayedMembers.length;
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isRemainingDropdownOpen, setIsRemainingDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const dropdownOptions = [
+    { label: 'Edit Details', onClick: () => console.log('Edit Details clicked') },
+    { label: 'View Details', onClick: () => console.log('View Details clicked') },
+    { label: 'Delete', onClick: () => console.log('Delete clicked') },
+  ];
+
   return (
     <div
       className={`${
-        project ? "shadow" : "border border-dark"
-      } rounded h-100 flex justify-center p-3 items-center`}
+        project ? "projectCard-shadow" : "border border-dark"
+      } rounded h-100 flex justify-center tems-center`}
     >
       {project ? (
-        <div>
-          <img src={project.image} alt="project" />
-          <h2>{project.name}</h2>
-          <p dangerouslySetInnerHTML={{ __html: project.description }} />
+        <div className="bg-[#FAFBFC] rounded-lg p-4 flex flex-col space-y-4 w-full relative">
+          <div className="flex justify-between">
+            <img
+              className="w-20 h-20 rounded-full object-cover"
+              src="https://via.placeholder.com/180"
+              alt="Profile"
+            />
+             <CustomDropdown
+              isOpen={isDropdownOpen}
+              toggleDropdown={toggleDropdown}
+              options={dropdownOptions}
+            />
+          </div>
+          <div className="flex justify-between">
+            <div>
+              <h2 className="text-base font-lato text-[#323333] font-semibold">
+                {project?.name}
+              </h2>
+              <p className="text-[11px] font-lato text-[#989CA6]">
+                Created by Hani Hassan |{" "}
+                {moment(project?.start_date).format("DD-MM-YY")}
+              </p>
+            </div>
+            <div className="flex -space-x-2.5 h-10">
+              {displayedMembers.map((member) => (
+                <span
+                  className={`${getRandomColor()} font-lato flex justify-center items-center text-[10.5px] font-bold text-[#FAFBFC] w-8 h-8 rounded-full`}
+                  key={member}
+                >
+                  <EmployeeName value={member} length={2} />
+                </span>
+              ))}
+              {remainingCount > 0 && (
+                <span
+                  className="bg-[#B6E5F9] font-lato flex justify-center items-center text-[10.5px] font-bold text-[#0D2282] w-8 h-8 rounded-full"
+                  key="remaining-count"
+                >
+                  +{remainingCount}
+                </span>
+              )}
+            </div>
+          </div>
         </div>
       ) : (
         <div
-          className="flex items-center flex-col cursor-pointer"
+          className="flex items-center justify-center flex-col cursor-pointer"
           onClick={() => {
             toggleAddProject();
           }}
