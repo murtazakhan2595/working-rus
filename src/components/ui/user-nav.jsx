0a02bf0171874765629b3fbd11481ca4523f148b
iamkashifkhn <usmankashif544@gmail.com>
2024-08-21 15:09:05 +0500

    added top bar integration

--- src/components/ui/user-nav.jsx
+++ src/components/ui/user-nav.jsx
@@ -22,15 +22,48 @@ import {
   DropdownMenuTrigger
 } from "../../src/@/components/ui/dropdown-menu";
 import { setUserLogout } from "../../state/actions/UserAction";
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import getNavigation from 'app/utils/Types/Navigation';
 
 export function UserNav() {
-
+  const [employee, setEmployee] = useState(null);
+  // const [profileImage, setProfileImage] = useState(null);
+  const token = window.localStorage.getItem("token");
+  const [Navigation, setNavigation] = useState(null);
+  const userProfile = useSelector((state) => state.user.userProfile);
+  const baseUrl = useSelector((state) => state.user.baseUrl);
   const navigate = useNavigate();
   const handleLogout = () => {
     window.localStorage.setItem("token", "");
     setUserLogout();
     navigate("/login");
   };
+
+  const fetchData = async () => {
+    const employeeResponse = await axios.get(
+      `${baseUrl}/emp/${userProfile.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const employeeData = employeeResponse.data;
+    setEmployee(employeeData);
+    setNavigation(getNavigation(employeeData.user_role));
+    // setProfileImage(
+    //   employeeResponse.data?.profile_picture?.file ||
+    //   employeeResponse.data?.profile_picture
+    // );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const combineFLName = employee?.first_name?.charAt(0).toUpperCase() +  employee?.last_name?.charAt(0).toUpperCase() 
 
   return (
     <DropdownMenu>
@@ -44,7 +77,7 @@ export function UserNav() {
               >
                 <Avatar className="w-8 h-8">
                   <AvatarImage src="#" alt="Avatar" />
-                  <AvatarFallback className="bg-transparent">JD</AvatarFallback>
+                  <AvatarFallback className="bg-transparent">{combineFLName}</AvatarFallback>
 
                 </Avatar>
               </Button>
@@ -57,9 +90,9 @@ export function UserNav() {
       <DropdownMenuContent className="w-56 " align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">John Doe</p>
+            <p className="text-sm font-medium leading-none">{employee?.account_title}</p>
             <p className="text-xs leading-none text-muted-foreground">
-              johndoe@example.com
+              {employee?.email}
             </p>
           </div>
         </DropdownMenuLabel>
