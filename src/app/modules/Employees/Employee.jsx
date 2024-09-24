b2986865e80b89d223823d2b7c04527d3559c537
Abdul Basit Ansari <abdulbasitansari288@gmail.com>
2024-09-24 16:22:16 +0500

    Handled All Statused in emp status Dropdown

--- src/app/modules/Employees/Employee.jsx
+++ src/app/modules/Employees/Employee.jsx
@@ -5,7 +5,7 @@ import CustomTable from "components/CustomTable";
 import { UsersRound, Contact, UserRoundCheck } from "lucide-react";
 import Header from "../../../components/Header";
 import { FilterInput, SelectComponent } from "components/form-control";
-import { UserRoles } from "data/Data";
+import { UserRoles, employeeStatus } from "data/Data";
 import {
   getDepartmentList,
   getDesignationList,
@@ -13,21 +13,18 @@ import {
 } from "app/hooks/general";
 import { PageLoader } from "components";
 import SheetOnBoarding from "../../../components/ui/OnBoardingSheet";
-import { Tabs, TabsContent } from "../../../src/@/components/ui/tabs";
 import Stats from "../../../components/ui/Stats";
 
 export default function EmployeeManagement() {
   const [isLoading, setIsLoading] = useState(true);
   const [employeeData, setEmployeeData] = useState({ results: [], count: 0 });
-  const [filteredEmployees, setFilteredEmployees] = useState([]);
-  const [empStatuses, setEmpStatuses] = useState(["all"]);
   const [departments, setDepartments] = useState([]);
   const [designations, setDesignations] = useState([]);
   const [filterData, setFilterData] = useState({});
   const [totalEmployee, setTotalEmployee] = useState(0);
   const [activeEmployee, setActiveEmployee] = useState(0);
   const [totalManagers, setTotalManagers] = useState(0);
-  const [selectedStatus, setSelectedStatus] = useState("all");
+  const [selectedStatus, setSelectedStatus] = useState("All");
   const [options, setOptions] = useState({ page: 1, sizePerPage: 10 });
 
   const onPageChange = (name, value) => {
@@ -46,20 +43,9 @@ export default function EmployeeManagement() {
       try {
         const data = await getEmployeeCustomList({ options, filterData });
         setEmployeeData(data);
-        setFilteredEmployees(
-          data.results.filter((employee) =>
-            selectedStatus === "all"
-              ? true
-              : employee.employee_status === selectedStatus
-          )
-        );
         setActiveEmployee(data.ActiveEmployee || 0);
         setTotalEmployee(data.TotalEmployee || 0);
         setTotalManagers(data.TotalManager || 0);
-        setEmpStatuses([
-          "all",
-          ...new Set(data.results.map((e) => e.employee_status)),
-        ]);
       } catch (error) {
         console.error("Error fetching employees:", error);
       } finally {
@@ -105,11 +91,7 @@ export default function EmployeeManagement() {
   ];
 
   const onEmpStatusChange = (newStatus) => {
-    setFilteredEmployees(
-      employeeData.results.filter((employee) =>
-        newStatus === "all" ? true : employee.employee_status === newStatus
-      )
-    );
+    handleFilterChange("employee_status", newStatus === "All" ? "" : newStatus);
     setSelectedStatus(newStatus);
   };
 
@@ -117,71 +99,58 @@ export default function EmployeeManagement() {
     <div className="flex flex-col gap-4 profile-management">
       <Header content={<SheetOnBoarding />} />
       <Stats stats={statsData} />
-      <Tabs
-        defaultValue="all"
-        className="w-full"
-        onValueChange={setSelectedStatus}
-      >
-        <div className="flex flex-col justify-between lg:flex-row md:flex-row xl:flex-row">
-          <SelectComponent
-            name="Employee Status"
-            value={selectedStatus}
-            options={empStatuses.map((status) => ({
-              value: status,
-              label: status.charAt(0).toUpperCase() + status.slice(1),
-            }))}
-            onChange={(name, newStatus) => onEmpStatusChange(newStatus)}
-            classes="flex-row"
-          />
-          <FilterInput
-            filters={[
-              {
-                type: "search",
-                placeholder: "Search by ID and Name",
-                name: "id_and_first_name",
-              },
-              {
-                type: "select-one",
-                option: departments,
-                name: "department_name",
-                placeholder: "Department",
-              },
-              {
-                type: "select-two",
-                option: designations,
-                name: "department_position",
-                placeholder: "Designation",
-              },
-              {
-                type: "select-three",
-                option: UserRoles,
-                name: "user_role",
-                placeholder: "Role",
-              },
-            ]}
-            onChange={handleFilterChange}
-          />
-        </div>
-        {empStatuses.map((status) => (
-          <TabsContent key={status} value={status}>
-            {isLoading ? (
-              <PageLoader />
-            ) : (
-              <Card>
-                <CardContent>
-                  <CustomTable
-                    data={filteredEmployees}
-                    columns={EmployeeColumns}
-                    pagination={true}
-                    dataTotalSize={employeeData.count || 0}
-                    tableOptions={tableOptions}
-                  />
-                </CardContent>
-              </Card>
-            )}
-          </TabsContent>
-        ))}
-      </Tabs>
+      <div className="flex flex-col justify-between lg:flex-row md:flex-row xl:flex-row">
+        <SelectComponent
+          name="Employee Status"
+          value={selectedStatus}
+          options={employeeStatus}
+          onChange={(name, newStatus) => onEmpStatusChange(newStatus)}
+          classes="flex-row"
+        />
+        <FilterInput
+          filters={[
+            {
+              type: "search",
+              placeholder: "Search by ID and Name",
+              name: "id_and_first_name",
+            },
+            {
+              type: "select-one",
+              option: departments,
+              name: "department_name",
+              placeholder: "Department",
+            },
+            {
+              type: "select-two",
+              option: designations,
+              name: "department_position",
+              placeholder: "Designation",
+            },
+            {
+              type: "select-three",
+              option: UserRoles,
+              name: "user_role",
+              placeholder: "Role",
+            },
+          ]}
+          onChange={handleFilterChange}
+        />
+      </div>
+      {isLoading ? (
+        <PageLoader />
+      ) : (
+        <Card>
+          <CardContent>
+            <CustomTable
+              data={employeeData.results}
+              columns={EmployeeColumns}
+              pagination={true}
+              dataTotalSize={employeeData.count || 0}
+              tableOptions={tableOptions}
+            />
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
