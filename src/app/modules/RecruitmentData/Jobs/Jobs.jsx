e0b43de1ecb845dc91c82eedab18f29889f118a0
iamkashifkhn <usmankashif544@gmail.com>
2024-11-07 13:14:06 +0500

    fixed ui issues in applications

--- src/app/modules/RecruitmentData/Jobs/Jobs.jsx
+++ src/app/modules/RecruitmentData/Jobs/Jobs.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from "react";
-import { BriefcaseIcon, CalendarIcon, UsersIcon } from "lucide-react";
+// import { BriefcaseIcon, CalendarIcon, UsersIcon } from "lucide-react";
 import Header from "../../../../components/Header";
 import { FilterInput } from "components/form-control";
 import { fetchJobPosts } from "app/hooks/recruitment";
@@ -24,9 +24,9 @@ import JobListingsTable from './JobListingsTable';
 
 import { CreateUpdateJob } from "..";
 
-const safeNumberDisplay = (value) => {
-  return Number.isNaN(value) || value === undefined || value === null ? '-' : value;
-};
+// const safeNumberDisplay = (value) => {
+//   return Number.isNaN(value) || value === undefined || value === null ? '-' : value;
+// };
 
 export default function JobsDataTable() {
   const [isLoading, setIsLoading] = useState(true);
