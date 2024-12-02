d04358d250849bf3ff6ed9c0f08673313f88d319
iamkashifkhn <usmankashif544@gmail.com>
2024-12-02 18:50:40 +0500

    fixed ui issues in organization

--- src/app/utils/FormSchema/organizationFormSchema.jsx
+++ src/app/utils/FormSchema/organizationFormSchema.jsx
@@ -7,6 +7,8 @@ const validateOrganizationSchema = (values, isEditMode) => {
     if (!values?.email) errors.email= "Email is required";
     if (!values?.address) errors.address= "Address is required";
     if (!values?.state) errors.state= "State is required";
+    if (!values?.city) errors.city= "City is required";
+    if (!values?.zipcode) errors.zipcode= "Zip/Postal Code is required";
     if (!values?.time_zone) errors.time_zone= "Timezone is required";
     if (!values?.date_format) errors.date_format= "Date Format is required";
     if (!values?.country) errors.country= "Country is required";
