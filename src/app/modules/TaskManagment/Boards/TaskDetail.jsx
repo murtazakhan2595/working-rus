fb55116d40fc7da6fbfa72d5a69fbd0dab1d1d6e
ajwatanveer <aleezy.tanv53@gmail.com>
2024-12-16 00:50:49 +0500

    Task Managment

--- src/app/modules/TaskManagment/Boards/TaskDetail.jsx
+++ src/app/modules/TaskManagment/Boards/TaskDetail.jsx
@@ -1,38 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
 import { CiEdit } from "react-icons/ci";
-import { getFileNameFromURL } from "utils/downUtils";
-import { getDarkerTextColor } from "./Sections/getTaskStatus";
-import { RxCross2, RxPerson } from "react-icons/rx";
-import {
-  AiOutlineDownload,
-  AiOutlineFile,
-  AiOutlinePaperClip,
-} from "react-icons/ai";
-import { FaRegImage } from "react-icons/fa";
-import { RiSendPlaneFill } from "react-icons/ri";
-import { LiaCommentAlt } from "react-icons/lia";
 import { PriorityList } from "data/Data";
 import moment from "moment";
 import {
   postComment,
   getBoardById,
-  getAttachmentById,
 } from "app/hooks/taskManagment";
-import { EmployeeName } from "utils/getValuesFromTables";
 import { connect, useSelector } from "react-redux";
 import EditCard from "./EditCard";
-import { getRandomColor } from "utils/renderValues";
 import { addCommentAttachment } from "app/hooks/taskManagment";
 import {
   getCommentsWithAttachments,
   getTaskById,
 } from "app/hooks/taskManagment";
-import { filebase64Download } from "utils/fileUtils";
 import { toast } from "react-toastify";
 import {
   Labels,
   MembersList,
-  Assignee,
   TaskComments,
   CheckList,
   Attachments,
@@ -43,25 +27,15 @@ import { Trash } from "lucide-react";
 
 import { DetailBox, DetailCard } from "components/SheetCardExtension";
 
-const TaskDetail = ({ taskId, onClose, employees, handleDelete }) => {
+const TaskDetail = ({ taskId, onClose, handleDelete }) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [comments, setComments] = useState([]);
   const [taskData, setTaskData] = useState({});
-  const [newComment, setNewComment] = useState("");
   const [boardName, setBoardName] = useState("Loading...");
-  const [attachments, setAttachments] = useState([]);
-  const [newAttachment, setNewAttachment] = useState({});
   const [isEditCardOpen, setIsEditCardOpen] = useState(false);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-  const [showDropdown, setShowDropdown] = useState(false);
-  const fileInputRef = useRef(null);
   const [isTaskDetailVisible, setIsTaskDetailVisible] = useState(true);
-  const commentRef = useRef(null);
-  const userId = useSelector((state) => state.user.userProfile.id);
 
   const fetchTaskData = async (isMounted) => {
     setIsLoading(true);
-
     try {
       // Fetch card details
       const cardDetails = await getTaskById(taskId);
@@ -90,15 +64,6 @@ const TaskDetail = ({ taskId, onClose, employees, handleDelete }) => {
     };
   }, [taskId]);
 
-  const fetchData = async () => {
-    try {
-      const data = await getCommentsWithAttachments({ task_id: [taskId] });
-      setComments(data);
-    } catch (error) {
-      console.error("Error fetching comments:", error);
-      setComments([]);
-    }
-  };
   useEffect(() => {
     const fetchBoardName = async () => {
       try {
@@ -110,71 +75,7 @@ const TaskDetail = ({ taskId, onClose, employees, handleDelete }) => {
     };
 
     fetchBoardName();
-    fetchData();
-  }, [taskData?.board_id, taskData?.id, taskData?.attachment]);
-
-  const handleAddComment = async () => {
-    if (newComment.trim() || newAttachment) {
-      try {
-        if (newAttachment) {
-          console.log("newAttachment", newAttachment);
-          try {
-            const response = await addCommentAttachment(newAttachment);
-            console.log("attachmed response", response);
-            if (response) {
-              const payload = {
-                task_id: taskData.id,
-                user_id: userId,
-                comment: newComment,
-                commentattach: [response.id],
-              };
-              const result = await postComment(payload);
-              if (result) {
-                setNewAttachment([]);
-                setNewComment("");
-              }
-            }
-          } catch (error) {
-            console.error("Error uploading file:", error);
-          }
-        } else if (newComment.trim()) {
-          const payload = {
-            task_id: taskId,
-            user_id: userId,
-            comment: newComment,
-          };
-          await postComment(payload);
-          setNewComment("");
-        }
-        console.log("newAttachment", newAttachment);
-        fetchData();
-      } catch (error) {
-        console.error("Error posting comment or uploading attachment:", error);
-      }
-    }
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0]; // Get the single file
-
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (event) => {
-        const fileData = {
-          name: file.name, // File name
-          file: event.target.result, // Base64 data URL
-        };
-        setNewAttachment(fileData); // Update state with file data
-      };
-
-      reader.onerror = (error) => {
-        console.error("Error reading file:", error); // Handle errors
-      };
-
-      reader.readAsDataURL(file); // Read file as data URL
-    }
-  };
+  }, [taskData?.board_id]);
 
   const editDetails = () => {
     setIsEditCardOpen(true);
--- src/app/modules/TaskManagment/Sections/TaskComments.jsx
+++ src/app/modules/TaskManagment/Sections/TaskComments.jsx
@@ -1,30 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Plus, Search } from "lucide-react";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "../../../../src/@/components/ui/popover";
-import {
-  TextInput,
-  SelectComponent,
-  DateInput,
   InputComments,
 } from "components/form-control.jsx";
 import { EmployeeName } from "utils/getValuesFromTables";
-import { filebase64Download } from "utils/fileUtils";
 import moment from "moment";
-import { Button } from "components/ui/button";
-import {
-  AiOutlineDownload,
-  AiOutlineFile,
-  AiOutlinePaperClip,
-} from "react-icons/ai";
-import { Input } from "components/ui/input";
-import { Label } from "../../../../src/@/components/ui/label";
-import { Checkbox } from "../../../../src/@/components/ui/checkbox";
 import AttachmentUI from "components/ui/AttachmentUI";
-import axios from "axios";
 import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 import {
@@ -50,7 +30,11 @@ export default function TaskComments({ taskId }) {
   };
 
   useEffect(async () => {
-    fetchComments();
+    let isMounted = true;
+    if (taskId) fetchComments(isMounted);
+    return () => {
+      isMounted = false;
+    };
   }, [taskId]);
 
   const handleAddComment = async (comment, attachments) => {
