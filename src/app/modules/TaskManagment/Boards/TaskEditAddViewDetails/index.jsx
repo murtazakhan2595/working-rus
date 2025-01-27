e965a5323e14f52766187912457f28eb884e1822
Ahmed Murtaza <murtazakhan2595@gmail.com>
2025-01-27 13:09:45 +0500

    change attachments to accept multiple file types

--- src/app/modules/TaskManagment/Boards/TaskEditAddViewDetails/index.jsx
+++ src/app/modules/TaskManagment/Boards/TaskEditAddViewDetails/index.jsx
@@ -472,6 +472,7 @@ const TaskEditAddViewDetails = ({
                           await props.setFieldValue("attachment", attachment);
                           setIsEditMode(true);
                         }}
+                        acceptedFileTypes=".pdf,.png,.jpg,.jpeg"
                         error={props.errors.relation}
                         touch={props.touched.relation}
                       />
