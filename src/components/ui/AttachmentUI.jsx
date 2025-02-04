6e6aaf97b8e74c3e117cb533e63927fa894910eb
ajwatanveer <aleezy.tanv53@gmail.com>
2025-02-04 12:56:13 +0500

    Image Preview

--- src/components/ui/AttachmentUI.jsx
+++ src/components/ui/AttachmentUI.jsx
@@ -1,14 +1,7 @@
-import React from "react";
-import { downloadFile } from "utils/fileUtils";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Button } from "components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from "src/@/components/ui/dialog.jsx";
+import React, { useState, useRef, useEffect } from "react";
 import { imageFileType } from "app/utils/Types/General";
+import ImageDocPreview  from "components/ui/ImageDocPreview";
+
 export default function AttachmentUI({
   attachment,
   name,
@@ -18,20 +11,14 @@ export default function AttachmentUI({
   id = null,
   displayImageAttachment = true,
 }) {
-  const [viewAttachment, setViewAttachment] = React.useState(false);
-
+  const [viewAttachment, setViewAttachment] = useState(false);
   if (!attachment) return null;
 
-  // Extract file type for rendering
   const getFileType = (file) => {
-    let fileType;
-
-    if (file instanceof File) {
-      fileType = file.name.split(".").pop()?.toUpperCase();
-    } else {
-      fileType = file?.split(".").pop()?.toUpperCase();
-    }
-
+    let fileType =
+      file instanceof File
+        ? file.name.split(".").pop()?.toUpperCase()
+        : file?.split(".").pop()?.toUpperCase();
     return fileType && fileType.length > 4 ? fileType.slice(0, 4) : fileType;
   };
 
@@ -40,28 +27,20 @@ export default function AttachmentUI({
     attachment instanceof File ? URL.createObjectURL(attachment) : attachment;
 
   if (!displayImageAttachment && imageFileType.includes(fileType)) return null;
+
   return (
     <div key={key || ""}>
-      <div
-        className={`flex items-center justify-between w-full gap-2 p-4 my-1 border border-gray-400 rounded-lg`}
-      >
+      <div className="flex items-center justify-between w-full gap-2 p-4 my-1 border border-gray-400 rounded-lg">
         <div className="flex items-center w-[85%]">
           <div
             className="flex items-center justify-center w-9 h-9"
             style={{ minWidth: "2rem" }}
           >
-            {/* File preview based on type */}
             {imageFileType.includes(fileType) ? (
               <img
                 src={fileURL}
                 alt={name || "Attachment"}
                 className="w-9 h-9"
-                onLoad={(e) => {
-                  // Revoke object URL after load
-                  if (attachment instanceof File) {
-                    URL.revokeObjectURL(e.target.src);
-                  }
-                }}
               />
             ) : (
               <span className="text-xs font-semibold text-gray-800 bg-gray-300 rounded-lg w-full h-full flex items-center justify-center">
@@ -74,7 +53,7 @@ export default function AttachmentUI({
               onClick={(e) => {
                 e.preventDefault();
                 setViewAttachment(true);
-              }} // Wrap in an arrow function
+              }}
               className="hover:text-gray-700"
             >
               {name || "Attachment"}
@@ -106,66 +85,14 @@ export default function AttachmentUI({
           </button>
         </div>
       </div>
-      {viewAttachment && (
-        <Dialog open={viewAttachment} onOpenChange={setViewAttachment}>
-          <DialogContent className="w-[90vw] min-w-[90vw] max-h-[100%] h-[90vh] ">
-            <div className="flex flex-col max-h-[100%] max-w-[100%] overflow-hidden gap-4">
-              <div className="flex flex-row justify-between px-6">
-                <h6 className=""> {name || 'Attachment'}</h6>
-                <Button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    downloadFile(attachment, name);
-                  }}
-                  className="mr-3"
-                  variant="continue"
-                  size={"sm"}
-                >
-                  Download <AiOutlineDownload className="w-5 h-5" />
-                </Button>
-              </div>
 
-              <div className=" overflow-hidden mb-2 w-full">
-                {imageFileType.includes(fileType) ? (
-                  <img
-                    src={fileURL}
-                    alt={name || "Attachment"}
-                    style={{
-                      width: "auto",
-                      objectFit: "cover",
-                      // maxWidth:'100%',
-                      margin: "auto",
-                      maxHeight: "100%",
-                      minHeight: "100%",
-                    }}
-                  />
-                ) : (
-                  <iframe
-                    src={
-                      fileType === "pdf"
-                        ? fileURL
-                        : `https://docs.google.com/gview?url=${encodeURIComponent(
-                            fileURL
-                          )}&embedded=true`
-                    }
-                    style={{
-                      width: "100%",
-                      height: "auto",
-                      maxHeight: "80vh",
-                      border: "none",
-                    }}
-                    title={name}
-                    onLoad={(e) => {
-                      if (attachment instanceof File) {
-                        URL.revokeObjectURL(e.target.src); // Clean up object URL
-                      }
-                    }}
-                  />
-                )}
-              </div>
-            </div>
-          </DialogContent>
-        </Dialog>
+      {viewAttachment && (
+        <ImageDocPreview
+          attachment={attachment}
+          name={name}
+          isOpen={viewAttachment}
+          setIsOpen={setViewAttachment}
+        />
       )}
     </div>
   );
