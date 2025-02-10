33494bc6e67a91510412699c2b0b2d293cc2b56e
ajwatanveer <aleezy.tanv53@gmail.com>
2025-02-10 19:38:25 +0500

    Add List action dropdown

--- src/components/ViewOtions.jsx
+++ src/components/ViewOtions.jsx
@@ -1,56 +1,65 @@
 import React from "react";
 import { Button } from "components/ui/button";
+import { TooltipText } from "components";
 import { cn } from "src/@/lib/utils";
-import {
-  ListTodo,
-  LayoutGrid,
-} from "lucide-react";
+import { ListTodo, LayoutGrid } from "lucide-react";
 
 const VIEW_TYPES = {
   LIST: "list",
   GRID: "grid",
 };
 
-const ViewOptions = React.memo(({ activeView = "list", setActiveView = () => {} }) => {
-  console.log("member list render again");
-  const viewButtons = [
-    {
-      icon: ListTodo,
-      isActive: activeView === VIEW_TYPES.LIST,
-      onClick: () => setActiveView(VIEW_TYPES.LIST),
-    },
-    {
-      icon: LayoutGrid,
-      isActive: activeView === VIEW_TYPES.GRID,
-      onClick: () => setActiveView(VIEW_TYPES.GRID),
-    },
-  ];
+const ViewOptions = React.memo(
+  ({ activeView = "list", setActiveView = () => {} }) => {
+    console.log("member list render again");
+    const viewButtons = [
+      {
+        icon: ListTodo,
+        isActive: activeView === VIEW_TYPES.LIST,
+        onClick: () => setActiveView(VIEW_TYPES.LIST),
+        tooltipText: "List View",
+      },
+      {
+        icon: LayoutGrid,
+        isActive: activeView === VIEW_TYPES.GRID,
+        tooltipText: "Grid View",
+        onClick: () => setActiveView(VIEW_TYPES.GRID),
+      },
+    ];
 
-  const IconButton = ({ icon: Icon, isActive, onClick }) => {
+    const IconButton = ({ icon: Icon, isActive, onClick, tooltipText }) => {
+      return (
+        <Button
+          onClick={onClick}
+          variant="ghost"
+          size="icon"
+          className={cn(
+            "h-8 w-10 rounded",
+            isActive && "bg-fuchsia-50 text-plum-1100"
+          )}
+        >
+          <TooltipText
+            tooltipTriggerText={<Icon className="h-4 w-4" aria-hidden="true" />}
+            content={tooltipText}
+          />
+        </Button>
+      );
+    };
+
     return (
-      <Button
-        onClick={onClick}
-        variant="ghost"
-        size="icon"
-        className={cn("h-8 w-10 rounded", isActive && "bg-fuchsia-50 text-plum-1100")}
-      >
-        <Icon className="h-4 w-4" aria-hidden="true" />
-      </Button>
+      <div className="flex gap-1 items-center self-stretch p-1 my-auto bg-white rounded min-h-[40px] text-neutral-900">
+        {viewButtons.map((button, index) => (
+          <IconButton
+            key={index}
+            icon={button.icon}
+            isActive={button.isActive}
+            onClick={button.onClick}
+            tooltipText={button.tooltipText}
+          />
+        ))}
+      </div>
     );
-  };
-
-  return (
-    <div className="flex gap-1 items-center self-stretch p-1 my-auto bg-white rounded min-h-[40px] text-neutral-900">
-      {viewButtons.map((button, index) => (
-        <IconButton
-          key={index}
-          icon={button.icon}
-          isActive={button.isActive}
-          onClick={button.onClick}
-        />
-      ))}
-    </div>
-  );
-});
+  }
+);
 
 export default ViewOptions;
