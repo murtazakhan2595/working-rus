a8c8fd35c68684407f3b27c2ff8bf7e5e4a3e4be
ajwatanveer <aleezy.tanv53@gmail.com>
2025-02-18 16:06:22 +0500

    Project Bug

--- src/app/utils/MappingObjects/mapTaskManagementData.jsx
+++ src/app/utils/MappingObjects/mapTaskManagementData.jsx
@@ -16,25 +16,40 @@ export function mapTaskPayloadData(data) {
 }
 
 export function mapProjectPayloadData(data) {
-  const formData = new FormData();
-  for (const key in Project) {
-    if (data.hasOwnProperty(key) && data[key]) {
-      if (key === "profile") {
-        if (data[key] instanceof File) formData.append(key, data[key]);
-      } else if (Array.isArray(data[key])) {
-        const array = data[key];
-        if (array.length > 0) {
-          array.forEach((obj) => formData.append(key, obj));
-        } else {
-          formData.append(key, "[]"); // Or JSON.stringify([]) if backend expects JSON format
-        }
-      } else {
-        formData.append(key, data[key]);
-      }
-    }
-  }
 
-  return formData;
+   // Initialize an empty payload object
+   const payload = {};
+   // Iterate over the keys in the Task object
+   for (const key in Project) {
+     // Check if the key exists in the data object
+     if (data.hasOwnProperty(key) && data[key]) {
+       // Add the key and its value to the payload
+       payload[key] = data[key];
+     }
+   }
+ 
+   // Return the constructed payload
+   return payload;
+ 
+  // const formData = new FormData();
+  // for (const key in Project) {
+  //   if (data.hasOwnProperty(key) && data[key]) {
+  //     if (key === "profile") {
+  //       if (data[key] instanceof File) formData.append(key, data[key]);
+  //     } else if (Array.isArray(data[key])) {
+  //       const array = data[key];
+  //       if (array.length > 0) {
+  //         array.forEach((obj) => formData.append(key, obj));
+  //       } else {
+  //         formData.append(key, "[]"); // Or JSON.stringify([]) if backend expects JSON format
+  //       }
+  //     } else {
+  //       formData.append(key, data[key]);
+  //     }
+  //   }
+  // }
+
+  // return formData;
 }
 
 export function mapCustomFieldPayloadData(data, projectID = null, id) {
