1af734c36214e71161166596a48b7211e5042b66
Ahmed Murtaza <murtazakhan2595@gmail.com>
2025-03-21 08:02:06 +0500

    Fixed employee onboarding document bugs

--- src/app/hooks/employee.jsx
+++ src/app/hooks/employee.jsx
@@ -1123,7 +1123,7 @@ const getEmployeeDocsChecklist = async (payload) => {
       headers: headers(),
     });
     if (response.status === 200) {
-      return response;
+      return response.data;
     }
   } catch (error) {
     if (error?.response?.status === 401) {
