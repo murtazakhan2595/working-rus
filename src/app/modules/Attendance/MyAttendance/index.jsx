8840bb364596dde19c4e9c1965ab0a837639f48d
Ahmed Murtaza <murtazakhan2595@gmail.com>
2025-04-04 14:15:24 +0500

    revert commit

--- src/app/modules/Attendance/MyAttendance/index.jsx
+++ src/app/modules/Attendance/MyAttendance/index.jsx
@@ -28,6 +28,7 @@ import { GetDateRange } from "utils/renderValues";
 
 const Attendance = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [employeeShift, setEmployeeShift] = useState({});
   const [attendance, setAttendance] = useState(null);
   const [attendanceData, setAttendanceData] = useState([]);
@@ -35,22 +36,26 @@ const Attendance = () => {
   // const [activeTab, setActiveTab] = useState("day");
   const [reloadData, setReloadData] = useState(false);
   const [dateRange, setDateRange] = useState(null);
-  const [activeFilter, setactiveFilter] = useState("Month");
+  const [attendanceHistoryLoading, setAttendanceHistoryLoading] =
+    useState(false);
+  const [activeFilter, setactiveFilter] = useState("day");
 
   const userProfile = useSelector((state) => state.user.userProfile);
   const user_details = useSelector((state) => state.emp.user_details);
   const [filterData, setFilterData] = useState({
     employee_id: userProfile.id,
-    date_range: GetDateRange(activeFilter),
+    date: moment().format("YYYY-MM-DD"),
   });
 
   const getAttendanceList = async () => {
+    setAttendanceHistoryLoading(true);
     const attendanceData = await getAttendance({
       filterData: filterData,
     });
     if (attendanceData) {
       setAttendanceData(attendanceData.results);
     }
+    setAttendanceHistoryLoading(false);
   };
 
   const setAttendanceWithLocalTime = (attendance) => {
@@ -70,20 +75,20 @@ const Attendance = () => {
     });
 
     if (attendance && attendance.results.length > 0) {
-      const todayAttendance = attendance?.results[0];
-      setAttendance(todayAttendance);
+      setAttendance(attendance?.results[0]);
       const breakStatus = await getBreakStatus({
         filterData: {
           employee_id: userProfile.id,
-          attendance: todayAttendance.id,
+          attendance: attendance.results[0].id,
         },
       });
       setOnBreak(breakStatus);
     }
   };
-  const fetchShiftData = async (isMounted, shiftAssigned) => {
-    const shift = await getShiftById(shiftAssigned);
-    if (shift && isMounted) {
+  const fetchShiftData = async () => {
+    debugger
+    const shift = await getShiftById(user_details?.shift_assignment);
+    if (shift) {
       setEmployeeShift({
         shift_start_time: moment(
           moment(shift.starttime).format("HH:mm:ss"),
@@ -152,11 +157,11 @@ const Attendance = () => {
 
   useEffect(() => {
     let isMounted = true;
-    fetchShiftData(isMounted, user_details?.shift_assignment);
+    fetchShiftData(isMounted);
     return () => {
       isMounted = false;
     };
-  }, [user_details]);
+  }, []);
 
   useEffect(() => {
     getAttendanceList();
@@ -184,7 +189,7 @@ const Attendance = () => {
       payable_hours: "0",
       date: moment().format("YYYY-MM-DD"),
     };
-    const response = await saveAttendance(payload, user_details);
+    const response = await saveAttendance(payload,user_details);
     if (response) {
       toast.success("Shift started");
       setAttendanceWithLocalTime(response);
@@ -341,65 +346,100 @@ const Attendance = () => {
     navigate(`/attendance-reports/${userProfile?.id}`);
   };
 
+  console.log("FILTERDATAAAAAAA", filterData);
+
   return (
     <>
-      <div className="p-4 space-y-4">
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          <EmployeeSelfTimesheet
-            employeeShift={employeeShift}
-            attendance={attendance}
-            startShift={startShift}
-            pauseShift={pauseShift}
-            endShift={endShift}
-            OnBreak={onBreak}
-            reloadData={() => {
-              getAttendanceList();
-              fetchData();
-            }}
-            setOnBreak={setOnBreak}
-          />
-
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-plum-900">Statistics</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <HourlyStatistics userId={userProfile?.id} />
-            </CardContent>
-          </Card>
-          <RecentActivities attendance={attendance} />
-        </div>
-
-        <div className="flex gap-2 justify-between items-center">
-          <h3 className="text-2xl font-semibold leading-none tracking-tight flex flex-col space-y-1.5 p-6">
-            {" "}
-            <div className="text-plum-900">Attendance History</div>
-          </h3>
-          <div className="flex gap-2">
-            <DateRangeFilter
-              activeDateRange={activeFilter}
-              setDateRange={(dateRange) => {
-                setactiveFilter(dateRange);
-                handleFilterChange(dateRange);
+      {loading ? (
+        <PageLoader />
+      ) : (
+        <div className="p-4 space-y-4">
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <EmployeeSelfTimesheet
+              employeeShift={employeeShift}
+              attendance={attendance}
+              startShift={startShift}
+              pauseShift={pauseShift}
+              endShift={endShift}
+              OnBreak={onBreak}
+              reloadData={() => {
+                getAttendanceList();
+                fetchData();
               }}
+              setOnBreak={setOnBreak}
             />
-            <Button variant="outline" onClick={downloadAttendance}>
-              Download
-            </Button>
+
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-plum-900">Statistics</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <HourlyStatistics userId={userProfile?.id} />
+              </CardContent>
+            </Card>
+            <RecentActivities attendance={attendance} />
           </div>
+
+          <div className="flex gap-2 justify-between items-center">
+            <h3 className="text-2xl font-semibold leading-none tracking-tight flex flex-col space-y-1.5 p-6">
+              {" "}
+              <div className="text-plum-900">Attendance History</div>
+            </h3>
+            <div className="flex gap-2">
+              {/* <div className="flex items-center gap-2 text-lg font-normal text-slate-900">
+                {["day", "week", "month"].map((tab) => (
+                  <button
+                    key={tab}
+                    className={`p-2 rounded-sm hover:bg-plum-400 hover:text-plum-900 ${
+                      activeTab === tab ? "bg-plum-400 text-plum-900" : ""
+                    }`}
+                    onClick={() => {
+                      setActiveTab(tab);
+                      handleFilterChange(tab);
+                    }}
+                  >
+                    {tab.charAt(0).toUpperCase() + tab.slice(1)}
+                  </button>
+                ))}
+                <DateRangePicker
+                  value={dateRange}
+                  onChange={(value) => {
+                    console.log("date range changed", value);
+                    setDateRange(value);
+                    handleFilterChange("date_range", value);
+                  }}
+                  placeholder="Select date range"
+                />
+              </div> */}
+              <DateRangeFilter
+                activeDateRange={activeFilter}
+                setDateRange={(dateRange) => {
+                  setactiveFilter(dateRange);
+                  handleFilterChange(dateRange);
+                }}
+              />
+              <Button variant="outline" onClick={downloadAttendance}>
+                Download
+              </Button>
+            </div>
+          </div>
+          <Card>
+            {attendanceHistoryLoading ? (
+              <PageLoader />
+            ) : (
+              <CardContent>
+                <TableCustom
+                  data={attendanceData}
+                  columns={MyAttendanceColumn}
+                  pagination={true}
+                  dataTotalSize={attendanceData.count || 0}
+                  // tableOptions={tableOptions}
+                />
+              </CardContent>
+            )}
+          </Card>
         </div>
-        <Card>
-          <CardContent>
-            <TableCustom
-              data={attendanceData}
-              columns={MyAttendanceColumn}
-              pagination={true}
-              dataTotalSize={attendanceData.count || 0}
-             // tableOptions={tableOptions}
-            />
-          </CardContent>
-        </Card>
-      </div>
+      )}
     </>
   );
 };
