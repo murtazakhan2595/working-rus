f6a94756c58787af896ddfce3734775551b9df89
ajwatanveer <aleezy.tanv53@gmail.com>
2025-04-06 15:07:22 +0500

    Merge Conflict

--- src/app/modules/Attendance/Sections/DepartmentOverview.jsx
+++ src/app/modules/Attendance/Sections/DepartmentOverview.jsx
@@ -17,7 +17,6 @@ const DepartmentOverview = () => {
       if (isMounted) setIsLoading(false);
     }
   };
-console.log()
   useEffect(() => {
     let isMounted = true;
     fetchData(isMounted);
@@ -25,30 +24,36 @@ console.log()
       isMounted = false;
     };
   }, []);
+  if (!departmentAttendanceData || departmentAttendanceData.length === 0) {
+  }
   return (
     <div className="p-4 bg-white border rounded-md shadow-md min-w-[33%]">
       <h2 className="text-xl font-semibold text-plum-900 mb-4">
         Department Overview
       </h2>
       <ul className="space-y-4">
-        {departmentAttendanceData.map((dept, index) => (
-          <li key={index} className="flex items-center justify-between">
-            <div className="flex-1">
-              <p className="text-sm font-medium text-gray-1000">
-                {dept.department_name}
-              </p>
-              <p className="text-xs text-gray-800">
-                {dept.total_employees} members • {dept.attendance_percentage}%
-                attendance
-              </p>
-              <Progress
-                value={dept.attendance_percentage}
-                className="mt-1 h-2 bg-gray-500"
-                color="purple"
-              />
-            </div>
-          </li>
-        ))}
+        {departmentAttendanceData && departmentAttendanceData.length > 0 ? (
+          departmentAttendanceData.map((dept, index) => (
+            <li key={index} className="flex items-center justify-between">
+              <div className="flex-1">
+                <p className="text-sm font-medium text-gray-1000">
+                  {dept.department_name}
+                </p>
+                <p className="text-xs text-gray-800">
+                  {dept.total_employees} members • {dept.attendance_percentage}%
+                  attendance
+                </p>
+                <Progress
+                  value={dept.attendance_percentage}
+                  className="mt-1 h-2 bg-gray-500"
+                  color="purple"
+                />
+              </div>
+            </li>
+          ))
+        ) : (
+          <></>
+        )}
       </ul>
     </div>
   );
