d0200a7858e023e5cdd71ec62ef3e258c853c4b0
ajwatanveer <aleezy.tanv53@gmail.com>
2025-05-08 13:22:24 +0500

    Stagging blocker

--- src/app/modules/Attendance/Sections/AttendanceTableColumns.jsx
+++ src/app/modules/Attendance/Sections/AttendanceTableColumns.jsx
@@ -60,7 +60,7 @@ export const EmployeesAttendanceColumns = (
     dataField: "attendance_stats",
     text: "Absent Days",
     formatter: (cell) => (
-      <span>{parseInt(TotalDays) - parseInt(cell?.Present)}</span>
+      <span>{Math.max(0, parseInt(TotalDays) - parseInt(cell?.Present))}</span>
     ),
   },
   {
