d854fe0fc7547e6050a98420f961a7dacee6b386
ajwatanveer <aleezy.tanv53@gmail.com>
2025-05-27 10:30:20 +0500

    Default Role Caanot be removed from the user

--- src/app/modules/RoleAndPermissions/AssignedRoles/AssignRoleForm.jsx
+++ src/app/modules/RoleAndPermissions/AssignedRoles/AssignRoleForm.jsx
@@ -87,11 +87,10 @@ const AssignRoleForm = ({ isOpen, setIsOpen, edit, reload }) => {
     if (!employeeData && !values.employee) {
       errors.employee = "Employee is required";
     }
-    if (!isEditMode) {
-      if (!values.roles || values.roles.length === 0) {
-        errors.roles = "At least one role must be selected";
-      }
-    }
+
+    // if (!values.roles || values.roles.length === 0) {
+    //   errors.roles = "At least one role must be selected";
+    // }
 
     return errors;
   };
@@ -99,7 +98,7 @@ const AssignRoleForm = ({ isOpen, setIsOpen, edit, reload }) => {
   const handleSubmit = async (values) => {
     const payload = {
       employeeId: employeeData?.id || employeeData?.value || values.employee,
-      roles: [...values.roles, default_role],
+      roles: [...values.roles,default_role],
     };
 
     setFormValues({ ...values, ...payload });
@@ -328,7 +327,7 @@ const AssignRoleForm = ({ isOpen, setIsOpen, edit, reload }) => {
                 {
                   InputField: SelectMultiInputComponent,
                   name: "roles",
-                  required: !isEditMode,
+                  // required: true,
                   label: "Select Roles",
                   options: roleOptions,
                   placeholder: "Select roles",
