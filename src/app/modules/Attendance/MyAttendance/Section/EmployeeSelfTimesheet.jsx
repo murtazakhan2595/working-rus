ac0a54c93ef1de488627680ad611c59e4b40ce19
ajwatanveer <aleezy.tanv53@gmail.com>
2025-06-03 14:08:48 +0500

    Integerated second shift check in

--- src/app/modules/Attendance/MyAttendance/Section/EmployeeSelfTimesheet.jsx
+++ src/app/modules/Attendance/MyAttendance/Section/EmployeeSelfTimesheet.jsx
@@ -52,14 +52,14 @@ export default function EmployeeSelfTimesheet({
 
   useEffect(() => {
     const isSplitShit = today_shift?.is_split_shift;
-    if (!OnBreak) {
-      if (attendance.checkin && !attendance.checkout) {
+    if (!OnBreak && attendance) {
+      if (attendance?.checkin && !attendance.checkout) {
         updateTimer(isSplitShit); // Initial update
         const interval = setInterval(updateTimer, 1000); // Update every second
         return () => clearInterval(interval); // Cleanup on unmount
       } else if (
         isSplitShit &&
-        attendance.second_checkin &&
+        attendance?.second_checkin &&
         !attendance.second_checkout
       ) {
         updateTimer(isSplitShit); // Initial update
