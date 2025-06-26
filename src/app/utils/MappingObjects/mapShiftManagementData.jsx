a4826cfd674e38890f2fa2cc2ee57e8408a979a3
ajwatanveer <aleezy.tanv53@gmail.com>
2025-06-26 20:08:24 +0500

    Wrong Total Time calculation

--- src/app/utils/MappingObjects/mapShiftManagementData.jsx
+++ src/app/utils/MappingObjects/mapShiftManagementData.jsx
@@ -92,7 +92,7 @@ export function mapDefaultShiftData(data) {
         endTime: endTime,
       },
     ];
-    active_shift.total_hours = CalculateTotalWorkingHours(endTime, endtime);
+    active_shift.total_hours = CalculateTotalWorkingHours(startTime, endTime);
   }
   return active_shift;
 }
