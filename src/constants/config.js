f0adc9e5046a9ce1b92c3b227c144ce17197743a
ajwatanveer <aleezy.tanv53@gmail.com>
2025-07-02 16:36:44 +0500

    Production Bugs

--- src/constants/config.js
+++ src/constants/config.js
@@ -20,8 +20,8 @@ const Config = {
   //----------------TEAM_MANAGEMENT------------
   TEAM_MANAGEMENT: true,
   //Sub Modules
-  TEAM_PROFILE: true,
-  TEAM_ATTENDANCE: true,
+  TEAM_PROFILE: false,
+  TEAM_ATTENDANCE: false,
   TEAM_DAILY_TASK_REPORT: false,
   TEAM_LEAVE_REQUEST: true,
   TEAM_EXIT_CLEARANCE: true,
@@ -66,10 +66,10 @@ const Config = {
   PROJECT_BOARD: false,
 
   //-------------TALENT_SPHERE-----------
-  TALENT_SPHERE: true,
+  TALENT_SPHERE: false,
   //Sub Modules
-  JOBS: true,
-  APPLICANTS: true,
+  JOBS: false,
+  APPLICANTS: false,
 
   //-------------ASSET_MANAGEMENT-----------
   ASSET_MANAGEMENT: true,
