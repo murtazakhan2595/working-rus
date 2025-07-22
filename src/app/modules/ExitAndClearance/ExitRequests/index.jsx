0e7f4ab54481538bd744a0063c005312fe5c19d6
ajwatanveer <aleezy.tanv53@gmail.com>
2025-07-22 15:40:39 +0500

    Employee filters and Sortings

--- src/app/modules/ExitAndClearance/ExitRequests/index.jsx
+++ src/app/modules/ExitAndClearance/ExitRequests/index.jsx
@@ -1,10 +1,6 @@
 import ExitRequests from "./ExitRequests";
-import Resignations from "./Resignations";
-import Terminations from "./Terminations";
 import RenderTerminationAction from "./Sections/RenderTerminationAction";
 export {
   ExitRequests,
-  Resignations,
-  Terminations,
   RenderTerminationAction,
 };
