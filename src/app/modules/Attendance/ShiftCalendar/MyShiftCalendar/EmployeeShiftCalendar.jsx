cf9a096a72f01831fbfa1e58f3ca9b1655901592
ajwatanveer <aleezy.tanv53@gmail.com>
2025-07-31 15:07:21 +0500

    Cannot approve the scheduled shift

--- src/app/modules/Attendance/ShiftCalendar/MyShiftCalendar/EmployeeShiftCalendar.jsx
+++ src/app/modules/Attendance/ShiftCalendar/MyShiftCalendar/EmployeeShiftCalendar.jsx
@@ -549,7 +549,7 @@ const EmployeeShiftCalendar = () => {
       return updatedFilters;
     });
   };
-
+console.log(isRequestChangeShiftPermitted,directShift,scheduleShifts,'hbvjhdf')
   return (
     <div className="space-y-6">
       {/* Header */}
