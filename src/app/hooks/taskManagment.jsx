e01427b8923608e692a460ef45eaa42ccb385634
ahmedtecbrix01 <malik.ahmed@tecbrix.com>
2025-09-01 15:28:54 +0500

    Update taskManagment.jsx

--- src/app/hooks/taskManagment.jsx
+++ src/app/hooks/taskManagment.jsx
@@ -189,7 +189,7 @@ export const getAllLabels = async (payload) => {
 
     if (response.status === 200) {
       const data = response.data?.results || [];
-      return data.filter(dta => dta?.project_id != null);
+      return data.filter(dta => dta?.project_id === payload);
     } else {
       return [];
     }
