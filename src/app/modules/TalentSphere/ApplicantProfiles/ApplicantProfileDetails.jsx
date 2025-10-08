f3085ff6c23e8fbcb431731566a61e878c66ec56
Ahmed Murtaza <murtazakhan2595@gmail.com>
2025-10-08 16:36:52 +0500

    Update navigation routes in TalentSphere components
    Corrected navigationLink in ApplicantProfileDetails and fixed route in KPICards to ensure proper routing within the TalentSphere module.

--- src/app/modules/TalentSphere/ApplicantProfiles/ApplicantProfileDetails.jsx
+++ src/app/modules/TalentSphere/ApplicantProfiles/ApplicantProfileDetails.jsx
@@ -57,7 +57,7 @@ export default function ApplicantProfileDetails() {
         <div className="flex flex-col gap-4">
             <Header
                 showBackButton={true}
-                navigationLink={'/talent-sphere/applicant-management-profile'}
+                navigationLink={'/talent-sphere/applicants-profile'}
                 content={ApplicantData.status === 'hired' && (
                     <Button onClick={handleCreateEmployee}>Create Employee</Button>
                 )}
