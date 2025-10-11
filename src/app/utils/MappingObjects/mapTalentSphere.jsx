1214520060af03867e62ca38859a5ab805eb432a
ajwatanveer <aleezy.tanv53@gmail.com>
2025-10-11 15:25:15 +0500

    Offer Blocker

--- src/app/utils/MappingObjects/mapTalentSphere.jsx
+++ src/app/utils/MappingObjects/mapTalentSphere.jsx
@@ -1042,6 +1042,16 @@ export async function mapOfferLetterData(data, fetchApprovalDetails = true) {
             }
         }
     }
+    if (['approved', 'rejected'].includes(data['status']?.toLowerCase())) {
+        const logs = data['approval_logs']?.[0];
+        if (logs.action_type?.toUpperCase() === 'APPROVED') {
+            RecordDetails['approved_by'] = logs.changed_by;
+            RecordDetails['approved_on'] = logs.timestamp;
+        } else if (logs.action_type?.toUpperCase() === 'REJECTED') {
+            RecordDetails['rejected_by'] = logs.changed_by;
+            RecordDetails['rejected_on'] = logs.timestamp;
+        }
+    }
     return RecordDetails;
 }
 export async function mapOfferLetterList(data) {
