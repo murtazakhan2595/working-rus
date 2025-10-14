175066f1785b5b6075cd2465ab424f558d61c3a9
ajwatanveer <aleezy.tanv53@gmail.com>
2025-10-14 22:10:30 +0500

    Bugs Talent Sphere

--- src/app/modules/TalentSphere/ApplicantProfiles/ExportProfile.jsx
+++ src/app/modules/TalentSphere/ApplicantProfiles/ExportProfile.jsx
@@ -1,13 +1,15 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { usePermissions } from "utils/PermissionUtils";
-import { getApplicantsList } from "app/hooks/talentSphere";
+import { getApplicantsList, getApplicantsData } from "app/hooks/talentSphere";
 import { Button } from "components/ui/button";
 import { exportRecordToExcel } from "utils/downloadUtils";
 import { renderRange, renderDate } from "utils/renderValues";
-import { Currency } from "utils/getValuesFromTables";
+import { ExportApplicantsRecord } from "app/modules/TalentSphere/Sections";
+import { GetDispatchStateList } from "utils/Lists";
 
-export default function ExportProfile({ filterData }) {
+export default function ExportProfile({ filterData, variant = 'all-profiles', applicant_id }) {
     const { hasAccess } = usePermissions();
+    const Currencies = GetDispatchStateList("currencies", "common");
     const exportPermitted = hasAccess("EXPORT_APPLICANT_DETAILS");
     const [isExporting, setIsExporting] = useState(null);
 
@@ -17,12 +19,15 @@ export default function ExportProfile({ filterData }) {
         setIsExporting(true);
         try {
             event.preventDefault();
-            const response = await getApplicantsList({
-                filterData: { ...filterData },
-                ordering: "-id",
-            });
+            const response = variant === "all-profiles" ?
+                await getApplicantsList({
+                    filterData: { ...filterData },
+                    ordering: "-id",
+                }) :
+                await getApplicantsData(applicant_id);
+
             if (response) {
-                const ResponseData = response.results;
+                const ResponseData = response.results ?? [response];
                 if (
                     !ResponseData ||
                     !Array.isArray(ResponseData) ||
@@ -32,28 +37,7 @@ export default function ExportProfile({ filterData }) {
                 } else {
                     const dataToExport = await Promise.all(
                         ResponseData?.map(async (row) => {
-                            return {
-                                ID: row.serial_id,
-                                'Candidate Name': row.candidate_id,
-                                'Candidate Name': row.candidate_name,
-                                Status: row.status,
-                                Email: row.email,
-                                'Contact Number': row.contact_number,
-                                'Application Source': row.application_source,
-                                'Emiratization Flag': row.emiratization_flag,
-                                'Job Position': row.job_title,
-                                Department: row.vacancy_department,
-                                'Application Date': row.application_date,
-                                Location: row.location,
-                                'Screened By': row.screened_by,
-                                'Screened Date': row.screened_date,
-                                'AI Match Score': row.ai_match_score,
-                                'AI Match Skills': row.ai_matched_skills,
-                                'AI Missing Skills': row.ai_missing_skills,
-                                'AI Suggested': row.ai_suggested ? 'Yes' : 'No',
-                                'Joining Date': renderDate(row.offers_tracking?.[0]?.joining_date),
-                                // 'Offered Salary': `${Currency({ value: row?.publish_vacancy?.currency })} (${row?.publish_vacancy?.payment_frequency || ""})`
-                            };
+                            return ExportApplicantsRecord(row, Currencies)
                         })
                     );
                     exportRecordToExcel(
